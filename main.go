604f899f005cf5fb1b4e793f1b26e790a6db8935
water <linshy76@mail2.sysu.edu.cn>
2025-04-11 08:37:18 +0800

    ...

--- main.go
+++ main.go
@@ -26,17 +26,13 @@ func webPush(c *gin.Context) {
 		// 服务器推送信息过来了
 		fmt.Println("Received type:", payload.Type)
 		fmt.Println("Received message:", payload.Data)
-		if strings.Contains(payload.Type, "GROUP__AT_MESSAGE_CREATE") {
+		switch payload.Type {
+		case "GROUP_AT_MESSAGE_CREATE":
 			controller.HandleGroupAtCreate(c, &payload)
-		} else if strings.Contains(payload.Type, "AT_MESSAGE_CREATE") {
-			controller.HandleAtCreate(c, &payload)
-		}
-		// switch payload.Type {
-		// case "GROUP_AT_MESSAGE_CREATE":
-
-		// case "AT_MESSAGE_CREATE":
-		// 	controller.HandleAtCreate(c, &payload)
-		// }
+		default:
+			if strings.Contains(payload.Type, "AT_MESSAGE_CREATE") {
+				controller.HandleAtCreate(c, &payload)
+			}
 	case 13:
 		// 服务器验证
 		controller.Validate(c, &payload)
