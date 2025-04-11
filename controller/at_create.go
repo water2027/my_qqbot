0cb77d7606cabbd5e0bc6886e81476b965446758
water <linshy76@mail2.sysu.edu.cn>
2025-04-11 08:29:22 +0800

    打印信息

--- controller/at_create.go
+++ controller/at_create.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 
 	"github.com/gin-gonic/gin"
 
@@ -30,11 +31,22 @@ func HandleAtCreate(c *gin.Context, payload *dto.Payload) {
 		msg := data.Content
 		token := service.AuthHelper.GetToken()
 		fmt.Println("获取token: ", token)
-		_, err := utils.NetHelper.POST(fmt.Sprintf("https://api.sgroup.qq.com/channels/%s/messages", msgId), dto.SendChannelMessage{
+		resp, err := utils.NetHelper.POST(fmt.Sprintf("https://api.sgroup.qq.com/channels/%s/messages", msgId), dto.SendChannelMessage{
 			Content: msg,
 			MsgId:   msgId,
 		}, utils.WithToken(token))
-		fmt.Println("发送消息", err)
+		if err != nil {
+			fmt.Println("发送消息失败", err)
+			return
+		}
+		res, err := io.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			fmt.Println("读取响应失败", err)
+			return
+		}
+
+		fmt.Println("发送消息", string(res), err)
 	}()
 
 	c.JSON(200, gin.H{
--- controller/group_at_create.go
+++ controller/group_at_create.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 
 	"github.com/gin-gonic/gin"
 
@@ -30,11 +31,22 @@ func HandleGroupAtCreate(c *gin.Context, payload *dto.Payload) {
 		msg := data.Content
 		token := service.AuthHelper.GetToken()
 		fmt.Println("获取token: ", token)
-		_, err := utils.NetHelper.POST(fmt.Sprintf("https://api.sgroup.qq.com/v2/groups/%s/messages", groupId), dto.SendGroupMessage{
+		resp, err := utils.NetHelper.POST(fmt.Sprintf("https://api.sgroup.qq.com/v2/groups/%s/messages", groupId), dto.SendGroupMessage{
 			Content: msg,
 			MsgType: 0,
 		}, utils.WithToken(token))
-		fmt.Println("发送消息", err)
+		if err != nil {
+			fmt.Println("发送消息失败", err)
+			return
+		}
+		res, err := io.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			fmt.Println("读取响应失败", err)
+			return
+		}
+
+		fmt.Println("发送消息", string(res), err)
 	}()
 
 	c.JSON(200, gin.H{
